ef2aed8528d9274683b3d1e00fb5fa37883d5267
horus <xiaoshewan@126.com>
2024-12-04 23:53:09 +0800

    interract with contract & add hardhat task use case

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,22 @@
 require("@nomicfoundation/hardhat-toolbox")
+
+// 在.env中明文配置参数时使用
 // require("dotenv").config()
+
+// 在.env.enc中加密配置相关参数时使用
 require("@chainlink/env-enc").config()
-// require("@nomicfoundation/hardhat-verify")
+require("@nomicfoundation/hardhat-verify")
+// require("./tasks/deploy-fundme")
+// require("./tasks/interact-fundme")
+// 自动加载 index.js
+require("./tasks")
 
 const SEPOLIA_URL = process.env.SEPOLIA_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY
+
+// 第二个账户的私钥（暂时没有）
+const PRIVATE_KEY_1 = process.env.PRIVATE_KEY_1
+
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 
 
@@ -17,7 +29,7 @@ module.exports = {
     sepolia: {
       // 从Alchemy,Infura,QuickNode等服务商网站上获取url
       url: SEPOLIA_URL,
-      accounts: [PRIVATE_KEY],
+      accounts: [PRIVATE_KEY,PRIVATE_KEY_1],
       chainId: 11155111
     }
   },
--- scripts/deployFundMe.js
+++ scripts/deployFundMe.js
@@ -13,23 +13,51 @@ async function main() {
     const fundMe = await fundMefactory.deploy(args)
     fundMe.waitForDeployment()
     console.log(`contract has been deployed successfully. contract adddesss is ${fundMe.target}`)
-   
-    if ( hre.network.config.chainId == 11155111 && process.env.ETHERSCAN_API_KEY ) {
+
+    if (hre.network.config.chainId == 11155111 && process.env.ETHERSCAN_API_KEY) {
         //等待5个区块确认后再做验证
         console.log("waiting for 5 confirmations")
-        await fundMe.deploymentTransaction().wait(5) 
-        verifyFundMe(fundMe.target,[args])
+        await fundMe.deploymentTransaction().wait(5)
+        verifyFundMe(fundMe.target, [args])
     } else {
         console.log("verification skiped.")
     }
+
+    // init 2 accounts
+    const [firstAccount, secondAccount] = await ethers.getSigners()
+
+    // fund contract with first account
+    const fundTx = await fundMe.fund({ value: ethers.parseEther("0.5") })
+    await fundTx.wait()
+
+    console.log(`2 accounts are ${firstAccount.address} and ${secondAccount.address}`)
+
+    // check balance of contract
+    const balanceOfContract = await ethers.provider.getBalance(fundMe.target)
+    console.log(`Balance of the contract is ${balanceOfContract}`)
+
+    // fund contract with second account
+    const fundTxWithSecondAccount = await fundMe.connect(secondAccount).fund({ value: ethers.parseEther("0.5") })
+    await fundTxWithSecondAccount.wait()
+
+    // check balance of contract
+    const balanceOfContractAfterSecondFund = await ethers.provider.getBalance(fundMe.target)
+    console.log(`Balance of the contract is ${balanceOfContractAfterSecondFund}`)
+
+    // check mapping 
+    const firstAccountbalanceInFundMe = await fundMe.fundersToAmount(firstAccount.address)
+    const secondAccountbalanceInFundMe = await fundMe.fundersToAmount(secondAccount.address)
+    console.log(`Balance of first account ${firstAccount.address} is ${firstAccountbalanceInFundMe}`)
+    console.log(`Balance of second account ${secondAccount.address} is ${secondAccountbalanceInFundMe}`)
+
 }
 
-async function verifyFundMe(fundMeAddr,args){
-     //自动化验证合约 run表示执行命令行命令
-     await hre.run("verify:verify", {
-         address: fundMeAddr,
-         constructorArguments: args,
-       });
+async function verifyFundMe(fundMeAddr, args) {
+    //自动化验证合约 run表示执行命令行命令
+    await hre.run("verify:verify", {
+        address: fundMeAddr,
+        constructorArguments: args,
+    });
 }
 
 main().then().catch((error) => {
